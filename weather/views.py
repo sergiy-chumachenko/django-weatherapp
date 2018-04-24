1b83fefb6eaff668cc8dfc1bcaac4cc8ee91a49a
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-24 21:12:20 +0300

    [UPDATE] Apply OpenWeatherMap API Part2

--- weather/views.py
+++ weather/views.py
@@ -1,17 +1,29 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&id=524901&APPID=f430bad4763e6508740bd70bb9d0e0b5'
-    city = 'Odessa'
 
-    r = requests.get(url.format(city)).json()
-    city_weather = {
-        'city': city,
-        'temperature': round(((r['main']['temp']-32) * 5/9), 2),
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
-    print(city_weather)
-    return render(request, 'weather/weather.html')
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
+
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': round(((r['main']['temp']-32) * 5/9), 2),
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/weather.html', context)
